fcc696f2e8a138b6f005051e3967bbdf6b51386a
jhessika-zarate <jhessikazarate@gmail.com>
2025-05-05 20:49:19 -0400

    2 rutas

--- src/assets/apiConfig.js
+++ src/assets/apiConfig.js
@@ -1,2 +1,3 @@
 const RutaApi = import.meta.env.VITE_BACKEND_URL || "http://localhost:2000/api/";
-export { RutaApi};
+const RutaApi2 = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api/";
+export { RutaApi,RutaApi2};
